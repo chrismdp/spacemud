94e90892a0031b92df0a413544a33d6fea06781b
Chris Parsons <chris@thinkcodelearn.com>
2014-05-09 15:47:16 +0100

    Extract guts of controller method

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,3 +1,20 @@
+class PlayerMover < Struct.new(:controller)
+  def move(player, location_id)
+    unless (destination = Location.find_by_id(location_id))
+      controller.flash[:error] = "That destination does not exist!"
+      controller.redirect_to controller.game_path
+      return
+    end
+
+    if (player.location.exits.any? { |e| e.destination == destination })
+      player.update_attributes!(location_id: location_id)
+    else
+      controller.flash[:error] = "You cannot move to that location from here"
+    end
+    controller.redirect_to controller.game_path
+  end
+end
+
 class GameController < ApplicationController
   before_filter :find_player, except: :index
 
@@ -9,15 +26,7 @@ class GameController < ApplicationController
   end
 
   def move
-    destination = Location.find_by_id(params[:destination_location_id])
-    if (destination.nil?)
-      flash[:error] = "That destination does not exist!"
-    elsif (@player.location.exits.any? { |e| e.destination == destination })
-      @player.update_attributes!(location_id: params[:destination_location_id])
-    else
-      flash[:error] = "You cannot move to that location from here"
-    end
-    redirect_to game_path
+    PlayerMover.new(self).move(@player, params[:destination_location_id])
   end
 
   private
