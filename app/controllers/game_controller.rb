7db1cf6c3cbc30efded33d0ad14ba8fbc611dc90
Chris Parsons <chris@thinkcodelearn.com>
2014-05-09 14:20:25 +0100

    updated README

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,15 +1,14 @@
 class GameController < ApplicationController
+  before_filter :find_player, except: :index
+
   def index
     @player = Player.new
   end
 
   def game
-    @player = Player.find_by_id(session[:player_id])
-    redirect_to root_path unless @player
   end
 
   def move
-    @player = Player.find_by_id(session[:player_id])
     destination = Location.find_by_id(params[:destination_location_id])
     if (destination.nil?)
       flash[:error] = "That destination does not exist!"
@@ -19,5 +18,12 @@ class GameController < ApplicationController
       flash[:error] = "You cannot move to that location from here"
     end
     redirect_to game_path
+  end
+
+  private
+
+  def find_player
+    @player = Player.find_by_id(session[:player_id])
+    redirect_to root_path unless @player
   end
 end
